1d3be5d8e13429b8d08a55856a512ea62f4bc928
Ben Newton <bennewton91@gmail.com>
2024-10-12 12:33:22 +0800

    added functionality to pop balls on click

--- script.js
+++ script.js
@@ -25,10 +25,9 @@ class Vector2D{
 
 class Ball{
     constructor(){
-
         this.position = new Vector2D(Math.random() * 370, Math.random() * 370);
-        this.velocity = new Vector2D(Math.random() * 3 + 2, Math.random() * 3 + 2);
-        this.size = 10;
+        this.velocity = new Vector2D(Math.random() * 2 + 2, Math.random() * 2 + 2);
+        this.radius = 20;
         this.color = this.fill_color();
     }
 
@@ -51,7 +50,7 @@ class Ball{
 
     draw_circle(){
         context.beginPath();
-        context.arc(this.position.getX(), this.position.getY(), this.size, 0, 2 * Math.PI, false);
+        context.arc(this.position.getX(), this.position.getY(), this.radius, 0, 2 * Math.PI, false);
         context.fillStyle = this.color;
         context.fill();
         context.closePath(); 
@@ -69,24 +68,56 @@ function add_first_ball(){
 
 add_first_ball();
 
+let counter = 1;
+
 function add_ball(){
     var boingSound = new Audio('./Sound/boing.mp3')
     boingSound.play();
     balls.push(new Ball);
+    counter ++;
 }
 
 
 const canvas = document.getElementById("game-box");
 var context = canvas.getContext("2d");
 
+canvas.addEventListener('click', function(event){
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+
+    balls.forEach((obj, index) => {
+        let dist = Math.sqrt((x - obj.position.getX()) ** 2 + (y - obj.position.getY()) ** 2);
+        console.log('worked')
+        if (dist < obj.radius + 10) {
+            let pop_sound = new Audio('./Sound/pop.mp3');
+            pop_sound.play();
+            balls.splice(index, 1); 
+            draw();
+        }
+    
+    });
+});
 
 function draw(){
     context.clearRect(0,0,canvas.width, canvas.height);
     for (let i = 0; i < balls.length; i++){
-        balls[i].update();
-        balls[i].draw_circle();
+        if (balls[i] != null){
+            balls[i].update();
+            balls[i].draw_circle();
+        } 
     }
 }
+
+function clear_ball(){
+    balls = [];
+    draw();
+}
+
+const clear_button = document.getElementById('clear-ball-button')
+clear_button.addEventListener('click', function(){
+    clear_ball();
+});
 
 
 function animate(){
